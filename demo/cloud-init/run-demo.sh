f3f36ccc8ac813e5dbbf8296842dac97f211795b
Yuri S <yuri.st@gmail.com>
2016-12-17 23:33:27 -0800

    monitor

--- demo/cloud-init/run-demo.sh
+++ demo/cloud-init/run-demo.sh
@@ -20,10 +20,26 @@ curdepth()
         tr -d '()'
 }
 
-echo
-echo '==========> mqlambdatm demo started'
-echo '==========> AWS_REGION='$AWS_REGION ' STACK_ID='$STACK_ID ' MYSQL_HOST='$MYSQL_HOST
-echo
+log()
+{
+    echo $1
+    aws sqs send-message --queue-url $SQS_QUEUE_URL --message-body "$1"
+}
+
+done_well()
+{
+    aws sqs send-message --queue-url $SQS_QUEUE_URL --message-body "<ok>"
+}
+
+done_badly()
+{
+    aws sqs send-message --queue-url $SQS_QUEUE_URL --message-body "<error>"
+}
+
+trap done_badly ERR
+
+log '==========> mqlambdatm demo started'
+log '==========> AWS_REGION='$AWS_REGION ' STACK_ID='$STACK_ID ' MYSQL_HOST='$MYSQL_HOST
 
 # install pre-requisites
 yum install -y docker mysql56
@@ -31,52 +47,54 @@ yum install -y docker mysql56
 # start docker service
 service docker start
 
-echo '==========> Cloning IBM MQ docker git repo'
+log '==========> Cloning IBM MQ docker git repo'
 pushd /tmp
 rm -rf mq-docker
 git clone https://github.com/ibm-messaging/mq-docker.git
 
 # build MQ server image with samples
-echo '==========> Building base MQ docker image'
+log '==========> Building base MQ docker image'
 # docker build -t mq-docker --build-arg MQ_PACKAGES="MQSeriesRuntime-*.rpm MQSeriesServer-*.rpm MQSeriesMsg*.rpm MQSeriesJava*.rpm MQSeriesJRE*.rpm MQSeriesGSKit*.rpm MQSeriesSamples*.rpm" ./mq-docker/server/
-echo '==========> Successfully build base MQ docker image'
+log '==========> Successfully build base MQ docker image'
 popd
 
-echo '==========> Building demo docker image'
+log '==========> Building demo docker image'
 docker build -t mqlambdatm-demo ../docker/
-echo '==========> Demo image built'
+log '==========> Demo image built'
 
 # wait for the stack creation to complete
-echo '==========> Waiting for stack creation to complete...'
+log '==========> Waiting for stack creation to complete...'
 aws cloudformation wait stack-create-complete --stack-name $STACK_ID
-echo '==========> Stack ready.'
+log '==========> Stack ready.'
 
-echo '==========> Creating sample database'
+log '==========> Creating sample database'
 mysql -h $MYSQL_HOST --user=demouser --password=demopass < ../sql/demo.sql
-echo '==========> Sample DB created'
+log '==========> Sample DB created'
 
-echo '==========> Running demo docker container'
+log '==========> Running demo docker container'
 docker run -d -p 1414:1414 -e LICENSE=accept -e MQ_QMGR_NAME=$QMGR_NAME -e AWS_REGION=$AWS_REGION -v /var/lambdademo:/var/mqm --name demo mqlambdatm-demo
 
 # Loop until "dspmq" says the queue manager is running
-echo '==========> Waiting for queue manager to come up'
+log '==========> Waiting for queue manager to come up'
 until [ "`state`" == "RUNNING" ]; do
     sleep 1
 done
-echo '==========> Queue manager running'
+log '==========> Queue manager running'
 
-echo '==========> Putting sample messages to the triggered queue'
+log '==========> Putting sample messages to the triggered queue'
 docker exec -i demo /opt/mqm/samp/bin/amqsput $TESTQ < ../sql/orders.txt
 
-echo '==========> Waiting for the queue to deplete'
+log '==========> Waiting for the queue to deplete'
 until [ `curdepth` == 0 ]; do
     sleep 5
 done
 
-echo "==========> Row counts from sample DB:"
-mysql -h $MYSQL_HOST --user=demouser --password=demopass mqlambdademo <<EOF
-select count(*) from orders;
-select count(*) from orderdetails;
+log "==========> Row counts from sample DB:"
+log "`mysql -h $MYSQL_HOST --user=demouser --password=demopass mqlambdademo <<EOF
+select count(*) as 'orders count' from orders;
+select count(*) as 'orderdetails count' from orderdetails;
 EOF
+`"
 
-echo "==========> mqlambdatm demo successfully completed"
+log "==========> mqlambdatm demo successfully completed"
+done_well
---
+++ demo/monitor.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+
+get() {
+    msg=$(aws sqs receive-message --queue-url $SQS_QUEUE_URL --wait-time-seconds 5)
+    if [[ "$msg" != "" ]]; then
+        receipt_handle=$(echo "$msg" | jq --raw-output .Messages[0].ReceiptHandle)
+        aws sqs delete-message --queue-url $SQS_QUEUE_URL --receipt-handle $receipt_handle
+        echo "$msg" | jq --raw-output .Messages[0].Body
+    else
+        echo ""
+    fi
+}
+
+msg=`get`
+
+until [[ "$msg" == "" || "$msg" == "<ok>" || "$msg" == "<error>" ]]; do
+    echo "$msg"
+done
+
+echo "$msg"
